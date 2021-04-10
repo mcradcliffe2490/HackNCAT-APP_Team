77e73a553f3b4338040904c50ccc6c5f4773ad9e
mycale radcliffe <Mcaws@MacBook-Air.home>
2021-04-10 01:20:27 -0400

    Fetches marvel data and returns it as a JSON object

--- fetchHeroData.js
+++ fetchHeroData.js
@@ -1,5 +1,20 @@
 import { API_PUBLIC_KEY, API_PRIVATE_KEY} from './configVault.js'
 
+
+// example: http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
+
+document.getElementById("button").addEventListener("click", fetchHeroData)
+
 function fetchHeroData (event) {
-    
+    let ts = event.timestamp;
+    let hash = md5(ts + API_PRIVATE_KEY + API_PUBLIC_KEY)
+    let siteRequest = "https://gateway.marvel.com/v1/public/characters?name="
+    let heroName = "Spider-Man" //Hardcoded right now, but will be the result of fetching a random hero name
+    let heroRequest = siteRequest + heroName + "&ts=" + ts + "&apikey=" + API_PUBLIC_KEY + "&hash=" + hash
+
+    fetch(heroRequest).then(result => result.json())
+        .then(result => {
+            console.log(result)
+    })
+
 }
